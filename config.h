ff54716f002362bf9d48fca50565cab321e59bd1
Jonas Zinn <Jonas.Zinn@uni-konstanz.de>
2014-10-16 18:51:38 +0200

    my windows config.h file

---
+++ config.h
@@ -0,0 +1,30 @@
+/*
+ * configuration for markdown, generated Thu Oct  9 21:44:04 GMT 2014
+ * by jonas@RattenHeim
+ */
+#ifndef __AC_MARKDOWN_D
+#define __AC_MARKDOWN_D 1
+
+
+#define OS_MINGW32_NT 1
+#define USE_DISCOUNT_DL 1
+#define while(x) while( (x) != 0 )
+#define if(x) if( (x) != 0 )
+#define DWORD unsigned long
+#define WORD unsigned short
+#define BYTE unsigned char
+#define HAVE_SRAND 1
+#define INITRNG(x) srand((unsigned int)x)
+#define HAVE_BZERO 1
+#define HAVE_RAND 1
+#define COINTOSS() (rand()&1)
+#define HAVE_STRCASECMP 1
+#define HAVE_STRNCASECMP 1
+#define HAVE_GETCWD 1
+#define TABSTOP 4
+#define HAVE_MALLOC_H 1
+#define PATH_FIND "/bin/find"
+#define PATH_SED "/bin/sed"
+#define WITH_FENCED_CODE 1
+
+#endif/* __AC_MARKDOWN_D */
